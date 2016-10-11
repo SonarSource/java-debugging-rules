73b472909bd69675aa89c3fe1bce80c887a19ee8
Michael Gumowski <michael.gumowski@sonarsource.com>
2016-10-11 17:36:24 +0200

    Update travis file

--- travis.sh
+++ travis.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
 set -euo pipefail
 
 function configureTravis { 
   mkdir ~/.local curl -sSL https://github.com/SonarSource/travis-utils/tarball/latest | tar zx --strip-components 1 -C ~/.local source ~/.local/bin/install
 }
-
 configureTravis
+
 . installJDK8
 
-mvn clean install
+# CI: build (incl. tests), deploy artifacts and triggers Dory analysis (incl. JaCoCo)
+regular_mvn_build_deploy_analyze
