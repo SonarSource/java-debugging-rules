47bc9719c2423669c8a7572a091279d96ba6cd03
Michael Gumowski <michael.gumowski@sonarsource.com>
2017-09-15 11:23:03 +0200

    Update to travis-utils V36

--- travis.sh
+++ travis.sh
@@ -3,12 +3,12 @@ set -euo pipefail
 
 function configureTravis { 
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v33 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v36 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
 
 . installJDK8
 
-# CI: build (incl. tests), deploy artifacts and triggers Dory analysis (incl. JaCoCo)
+# CI: build (incl. tests), deploy artifacts and triggers NEXT analysis (incl. JaCoCo)
 regular_mvn_build_deploy_analyze
